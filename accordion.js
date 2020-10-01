6b2245f50100bfc9b8b02a04943d6657257951c5
RF604 <raffi@freedman.ca>
2020-09-30 17:37:29 -0700

    periodic commit

--- accordion.js
+++ accordion.js
@@ -4,14 +4,21 @@ dropdown.forEach(section => {
   const toggler = section.querySelector(".toggler")
   const panel = section.querySelector(".panel")
 
-  toggler.addEventListener("click", function (event) {
+  toggler.addEventListener("click", function(event) {
     section.classList.toggle("open")
-      event.preventDefault()
+    event.preventDefault()
 
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px"
-      }
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px"
+    }
+
+    const images = dropdown.querySelectorAll("img")
+    images.forEach(img => {
+
+        img.setAttribute("src", img.getAttribute("data-src"))
+
+    })
   })
 })
